cd62d4b5395879337b5fc4c0564673772eeecbe7
IT22056320 <it22056320@my.sliit.lk>
2026-05-12 18:51:33 +0530

    feat: integrate Prisma with PostgreSQL and set up initial database schema - Added Prisma dependencies to package.json - Created prisma.config.ts for Prisma configuration - Defined initial database schema in schema.prisma - Added migration files for User and Application tables with foreign key constraints - Included migration lock file for version control

---
+++ server/prisma/migrations/20260512131608_init/migration.sql
@@ -0,0 +1,36 @@
+-- CreateEnum
+CREATE TYPE "Status" AS ENUM ('APPLIED', 'INTERVIEW', 'OFFER', 'REJECTED', 'WITHDRAWN');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Application" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "jobTitle" TEXT NOT NULL,
+    "company" TEXT NOT NULL,
+    "location" TEXT,
+    "status" "Status" NOT NULL DEFAULT 'APPLIED',
+    "notes" TEXT,
+    "appliedDate" TIMESTAMP(3) NOT NULL,
+    "followUpDate" TIMESTAMP(3),
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Application_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Application" ADD CONSTRAINT "Application_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
